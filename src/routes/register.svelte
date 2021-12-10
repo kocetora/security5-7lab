40768f435de55365ab0b4f77f78f4b04a2b1de40
v.rieznikova <v.rieznikova@nodeart.io>
2021-12-10 19:48:57 +0200

    register html

---
+++ src/routes/register.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    let username;
+    let email;
+    let password;
+</script>
+
+<form action="/register" method="POST">
+    <label for="username">
+        Name:
+        <input type="text" name="username" />
+    </label>
+    <label for="email">
+        Email:
+        <input type="email" name="email" />
+    </label>
+    <label for="password">
+        Password:
+        <input type="password" name="password" />
+    </label>
+    <input type="submit" value="Register">
+</form>
+
+<style>
+    label {
+        display: block;
+        margin-bottom: 20px;
+    }
+</style>
