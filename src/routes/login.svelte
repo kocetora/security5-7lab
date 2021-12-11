e75fb92141ff9972278c1ece7de07049ce5c0210
v.rieznikova <v.rieznikova@nodeart.io>
2021-12-11 02:12:15 +0200

    added error messages

--- src/routes/login.svelte
+++ src/routes/login.svelte
@@ -1,19 +1,25 @@
 <script lang="ts">
-    let email;
-    let password;
-
-    const submit = async () => {
-        const post = await fetch("http://localhost:4000/auth/login", {
-            method: 'POST',
-            body: JSON.stringify({
-                email,
-                password,
-            })
-        });
-
-        const data = await post.json();
-        console.log(data);
+let email;
+let password;
+let data;
+let error;
+const submit = async () => {
+    const post = await fetch("http://localhost:4000/auth/login", {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            email,
+            password,
+        })
+    });
+    if (post.ok) {
+        data = await post.json();
+        error = undefined;
+    } else {
+        error = await post.text();
+        data = undefined;
     }
+}
 </script>
 
 <form on:submit|preventDefault={submit}>
@@ -28,9 +34,30 @@
     <input type="submit" value="Login">
 </form>
 
+{#if data}
+    {#each Object.entries(data) as [title, paragraph]}
+        <h1>{title}</h1>
+        <p>{paragraph}</p>
+    {/each}
+{/if}
+
+{#if error}
+    <h1 class="error">{error}</h1>
+{/if}
+
 <style>
     label {
         display: block;
         margin-bottom: 20px;
+    }
+    h1 {
+        font-size: 20pt;
+    }
+    p {
+        margin: 0 auto;
+        font-size: 15pt;
+    }
+    .error {
+        color: red;
     }
 </style>
--- src/routes/register.svelte
+++ src/routes/register.svelte
@@ -1,21 +1,27 @@
 <script lang="ts">
-    let address;
-    let email;
-    let password;
-
-    const submit = async () => {
-        const post = await fetch("http://localhost:4000/auth/register", {
-            method: 'POST',
-            body: JSON.stringify({
-                address,
-                email,
-                password,
-            })
-        });
-
-        const data = await post.json();
-        console.log(data);
+let address;
+let email;
+let password;
+let data;
+let error;
+const submit = async () => {
+    const post = await fetch("http://localhost:4000/auth/register", {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            address,
+            email,
+            password,
+        })
+    });
+    if (post.ok) {
+        data = await post.json();
+        error = undefined;
+    } else {
+        error = await post.text();
+        data = undefined;
     }
+}
 </script>
 
 <form on:submit|preventDefault={submit}>
@@ -34,9 +40,30 @@
     <input type="submit" value="Register">
 </form>
 
+{#if data}
+    {#each Object.entries(data) as [title, paragraph]}
+        <h1>{title}</h1>
+        <p>{paragraph}</p>
+    {/each}
+{/if}
+
+{#if error}
+    <h1 class="error">{error}</h1>
+{/if}
+
 <style>
     label {
         display: block;
         margin-bottom: 20px;
+    }
+    h1 {
+        font-size: 20pt;
+    }
+    p {
+        margin: 0 auto;
+        font-size: 15pt;
+    }
+    .error {
+        color: red;
     }
 </style>
